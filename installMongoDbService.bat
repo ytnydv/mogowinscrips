237f3fa2cbb522aa0420e98fda8f7342ca37dd40
DESKTOP-N3H5L9N\Maxxx <ytn.ydv@gmail.com>
2017-07-04 19:51:57 +0530

    create basic directory structure to us for mogo db

--- installMongoDbService.bat
+++ installMongoDbService.bat
@@ -1,1 +1,4 @@
-"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --config "D:\CODE\mongodb\mongod.cfg" --install
+mkdir D:\mongodb\data
+mkdir D:\mongodb\data\log
+mkdir D:\mongodb\data\db
+"C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe" --config "D:\mongodb\mongod.cfg" --install
